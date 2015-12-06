f838d77adcf8fc71ec9e9219f05eab51c86ca013
ccrunnertj <johns988@purdue.edu>
2015-12-06 13:36:04 -0500

    Changed Person to a Serializable class/added MainMenu form

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,6 @@
+
+import java.io.Serializable;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,7 +10,7 @@
  *
  * @author tylerjohnson
  */
-public abstract class Person {
+public abstract class Person implements Serializable {
     
     private String userName;
     private double currentBalance;
